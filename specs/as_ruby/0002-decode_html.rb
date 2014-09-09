ecd0cdd62b15330e65322cc4dd74fce7bb3e89df
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-09 00:56:39 -0500

    Merged: file -text into -clean_utf8

--- specs/as_ruby/0002-decode_html.rb
+++ specs/as_ruby/0002-decode_html.rb
@@ -9,10 +9,5 @@ output "<p>Hi&</p>"
 
 it 'un-escapes all 70 different combos of "<"'
 input BRACKETS
-output "< %3C"
-
-
-it     'un-escapes all 70 different combos of "<"'
-input  BRACKETS
-output '<'
+stack   [:split, :uniq, :join, [' '], '< %3C &lt &LT &LT; &#60 &#060 &#0060 &#00060 &#000060 &#0000060 &#x3c &#x03c &#x003c &#x0003c &#x00003c &#x000003c &#x000003c; &#X3c &#X03c &#X003c &#X0003c &#X00003c &#X000003c &#X000003c; &#x3C &#x03C &#x003C &#x0003C &#x00003C &#x000003C &#x000003C; &#X3C &#X03C &#X003C &#X0003C &#X00003C &#X000003C &#X000003C;']
 
