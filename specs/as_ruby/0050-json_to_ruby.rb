fea6e930bfd7466675412c81440aa00eff7cc1fe
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-15 21:35:23 -0500

    Dev. checkpoint.

--- specs/as_ruby/0050-json_to_ruby.rb
+++ specs/as_ruby/0050-json_to_ruby.rb
@@ -2,3 +2,5 @@
 it     'strips unprintable chars'
 
 it     'raises Invalid if invalid JSON'
+
+it     'raises Invalid if not a String:'
--- specs/as_ruby/0051-ruby_to_json.rb
+++ specs/as_ruby/0051-ruby_to_json.rb
@@ -1,2 +1,17 @@
 
 it     'raises Invalid if it encounters an unallowed Object'
+
+
+it     'raises Invalid if not an Array:'
+input  {'a'=>'hello'}
+raises Escape_Escape_Escape::Invalid
+
+
+it     'raises Invalid if not an Array:'
+input  'a'
+raises Escape_Escape_Escape::Invalid
+
+
+it     'raises Invalid if not an Array:'
+input  1
+raises Escape_Escape_Escape::Invalid
